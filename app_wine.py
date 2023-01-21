b11a89b03f96927e51f37aab7fce85d7337b2eb6
Abhishek Tiwari <48113489+Ahbishek-analytics@users.noreply.github.com>
2023-01-21 14:29:30 +0530

    Add files via upload

--- app_wine.py
+++ app_wine.py
@@ -82,8 +82,9 @@ def main():
     df = pd.read_csv("./wine.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
     # df = pd.read_excel(...)  # will work for Excel files
 
-    st.title("Hello world!")  # add a title
-    st.write(df)  # visualize my dataframe in the Streamlit app
+    if st.button("Show data"):
+        st.title("Hello world!")  # add a title
+        st.write(df)  # visualize my dataframe in the Streamlit app
     
     
     if st.button("About"):
